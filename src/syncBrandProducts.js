c561e5235ca0799de5dd7084ceb81c4fb122b1f5
Brandon Bayer <b@bayer.ws>
2018-02-09 23:35:07 -0500

    new: update product sync

--- src/syncBrandProducts.js
+++ src/syncBrandProducts.js
@@ -1,6 +1,9 @@
 const algoliasearch = require('algoliasearch')
 
-const algolia = algoliasearch(process.env['ALGOLIA_APP_ID'], process.env['ALGOLIA_API_KEY'])
+const algolia = algoliasearch(
+  process.env['ALGOLIA_APP_ID'],
+  process.env['ALGOLIA_API_KEY'],
+)
 const index = algolia.initIndex(process.env['ALGOLIA_INDEX_NAME'])
 
 const modelName = 'Brand'
@@ -8,17 +11,17 @@ const modelName = 'Brand'
 module.exports = event => {
   if (!process.env['ALGOLIA_APP_ID']) {
     console.log('Please provide a valid Algolia app id!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   if (!process.env['ALGOLIA_API_KEY']) {
     console.log('Please provide a valid Algolia api key!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   if (!process.env['ALGOLIA_INDEX_NAME']) {
     console.log('Please provide a valid Algolia index name!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   const mutation = event.data[modelName].mutation
@@ -26,26 +29,28 @@ module.exports = event => {
   const previousValues = event.data[modelName].previousValues
 
   switch (mutation) {
-    case 'CREATED': return syncAddedNode(brand)
-    case 'UPDATED': return syncUpdatedNode(brand)
+    case 'CREATED':
+      return syncAddedNode(brand)
+    case 'UPDATED':
+      return syncUpdatedNode(brand)
     default:
       console.log(`mutation was '${mutation}'. Unable to sync node.`)
       return Promise.resolve()
   }
 }
 
-const translateGraphCoolToAlgolia = brand => (
+const translateGraphCoolToAlgolia = brand =>
   brand.products.map(product => ({
     objectID: product.id,
     name: product.name,
+    isAffiliate: product.isAffiliate,
     isHidden: product.isHidden,
     brandName: brand.name,
     brandBoost: brand.boost,
     brandIsHidden: brand.isHidden,
     brandId: brand.id,
-  })
-)
-
+    thrive: product.thriveListings,
+  }))
 
 function syncAddedNode(node) {
   console.log('Adding nodes')
--- src/syncProducts.js
+++ src/syncProducts.js
@@ -1,6 +1,9 @@
 const algoliasearch = require('algoliasearch')
 
-const algolia = algoliasearch(process.env['ALGOLIA_APP_ID'], process.env['ALGOLIA_API_KEY'])
+const algolia = algoliasearch(
+  process.env['ALGOLIA_APP_ID'],
+  process.env['ALGOLIA_API_KEY'],
+)
 const index = algolia.initIndex(process.env['ALGOLIA_INDEX_NAME'])
 
 const modelName = 'Product'
@@ -8,17 +11,17 @@ const modelName = 'Product'
 module.exports = event => {
   if (!process.env['ALGOLIA_APP_ID']) {
     console.log('Please provide a valid Algolia app id!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   if (!process.env['ALGOLIA_API_KEY']) {
     console.log('Please provide a valid Algolia api key!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   if (!process.env['ALGOLIA_INDEX_NAME']) {
     console.log('Please provide a valid Algolia index name!')
-    return { error: 'Module not configured correctly.' }
+    return {error: 'Module not configured correctly.'}
   }
 
   const mutation = event.data[modelName].mutation
@@ -26,9 +29,12 @@ module.exports = event => {
   const previousValues = event.data[modelName].previousValues
 
   switch (mutation) {
-    case 'CREATED': return syncAddedNode(product)
-    case 'UPDATED': return syncUpdatedNode(product)
-    case 'DELETED': return syncDeletedNode(previousValues)
+    case 'CREATED':
+      return syncAddedNode(product)
+    case 'UPDATED':
+      return syncUpdatedNode(product)
+    case 'DELETED':
+      return syncDeletedNode(previousValues)
     default:
       console.log(`mutation was '${mutation}'. Unable to sync node.`)
       return Promise.resolve()
@@ -36,15 +42,16 @@ module.exports = event => {
 }
 
 const translateGraphCoolToAlgolia = product => ({
-    objectID: product.id,
-    name: product.name,
-    isHidden: product.isHidden,
-    brandName: product.brand.name,
-    brandBoost: product.brand.boost,
-    brandIsHidden: product.brand.isHidden,
-    brandId: product.brand.id,
+  objectID: product.id,
+  name: product.name,
+  isAffiliate: product.isAffiliate,
+  isHidden: product.isHidden,
+  brandName: product.brand.name,
+  brandBoost: product.brand.boost,
+  brandIsHidden: product.brand.isHidden,
+  brandId: product.brand.id,
+  thrive: product.thriveListings,
 })
-
 
 function syncAddedNode(node) {
   console.log('Adding node')
