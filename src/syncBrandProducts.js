1c81b9b9f33222415974131de34e8a8fccba3fbe
Brandon Bayer <b@bayer.ws>
2018-02-20 12:15:10 -0500

    new: sync brand changes

--- src/syncBrandProducts.js
+++ src/syncBrandProducts.js
@@ -1,64 +1,36 @@
-const algoliasearch = require('algoliasearch')
+import {fromEvent} from 'graphcool-lib'
+const PQueue = require('p-queue')
+const queue = new PQueue({concurrency: 30})
 
-const algolia = algoliasearch(
-  process.env['ALGOLIA_APP_ID'],
-  process.env['ALGOLIA_API_KEY'],
-)
-const index = algolia.initIndex(process.env['ALGOLIA_INDEX_NAME'])
-
-const modelName = 'Brand'
-
-module.exports = event => {
-  if (!process.env['ALGOLIA_APP_ID']) {
-    console.log('Please provide a valid Algolia app id!')
-    return {error: 'Module not configured correctly.'}
+const mutation = `mutation($id: ID! $forceSyncTrigger: String!) {
+  updateProduct(
+		id: $id
+    forceSyncTrigger: $forceSyncTrigger
+	) {
+    id
   }
+}`
 
-  if (!process.env['ALGOLIA_API_KEY']) {
-    console.log('Please provide a valid Algolia api key!')
-    return {error: 'Module not configured correctly.'}
+export default async event => {
+  try {
+    const api = fromEvent(event).api('simple/v1')
+    const brand = event.data.Brand.node
+    const products = brand.products
+
+    for (let product of products) {
+      queue.add(
+        async () =>
+          await api.request(mutation, {
+            id: product.id,
+            forceSyncTrigger: new Date().toISOString(),
+          }),
+      )
+    }
+
+    await queue.onEmpty()
+
+    return {event: `Touched ${products.length} products`}
+  } catch (err) {
+    return {error: err}
   }
-
-  if (!process.env['ALGOLIA_INDEX_NAME']) {
-    console.log('Please provide a valid Algolia index name!')
-    return {error: 'Module not configured correctly.'}
-  }
-
-  const mutation = event.data[modelName].mutation
-  const brand = event.data[modelName].node
-  const previousValues = event.data[modelName].previousValues
-
-  switch (mutation) {
-    case 'CREATED':
-      return syncAddedNode(brand)
-    case 'UPDATED':
-      return syncUpdatedNode(brand)
-    default:
-      console.log(`mutation was '${mutation}'. Unable to sync node.`)
-      return Promise.resolve()
-  }
-}
-
-const translateGraphCoolToAlgolia = brand =>
-  brand.products.map(product => ({
-    objectID: product.id,
-    name: product.name,
-    isAffiliate: product.isAffiliate,
-    isHidden: product.isHidden,
-    brandName: brand.name,
-    brandBoost: brand.boost,
-    brandIsHidden: brand.isHidden,
-    brandId: brand.id,
-    thrive: product.thriveListings,
-    nuts: product.nutsListings,
-  }))
-
-function syncAddedNode(node) {
-  console.log('Adding nodes')
-  return index.addObjects(translateGraphCoolToAlgolia(node))
-}
-
-function syncUpdatedNode(node) {
-  console.log('Updating nodes')
-  return index.saveObjects(translateGraphCoolToAlgolia(node))
 }
